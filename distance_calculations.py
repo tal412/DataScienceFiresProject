9d7ad849761625b9f752ecb85101717a3dca6b74
Tal Barda <412tal412@gmail.com>
2024-02-22 18:14:32 +0200

    Improved distance features

---
+++ distance_calculations.py
@@ -0,0 +1,14 @@
+
+def calculate_distances_chunk(points_chunk, polygons_gdf, return_attribute=None):
+    polygons_sindex = polygons_gdf.sindex
+    results_list = []
+    for point in points_chunk:
+        result = polygons_sindex.nearest(point, return_distance=True, return_all=False)
+        nearest_idx = result[0][1][0]
+        distance = result[1][0] * 111  # Convert to kilometers
+        if return_attribute:
+            attribute_value = polygons_gdf.iloc[nearest_idx][return_attribute]
+            results_list.append((distance, attribute_value))
+        else:
+            results_list.append(distance)
+    return results_list
